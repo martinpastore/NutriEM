3c4baa6a9d89291d821891a79bb2bd2fed89aa96
Martin Pastore <martinpablopastore@gmail.com>
2018-11-23 14:22:00 -0300

    fix

---
+++ server.js
@@ -0,0 +1,19 @@
+equire('dotenv').config(); // read .env files
+const express = require('express');
+var router = require('./server/routes');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set public folder as root
+app.use(express.static('public'));
+
+// Allow front-end access to node_modules folder
+app.use('/scripts', express.static(`${__dirname}/node_modules/`));
+
+app.use('/', router);
+
+// Listen for HTTP requests on port 3000
+app.listen(port, () => {
+    console.log('listening on %d', port);
+});
