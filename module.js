c5acd199a22841314af29117f5ae31380187d959
Martin Pastore <martinpablopastore@gmail.com>
2018-11-24 02:01:57 -0300

    Header

--- module.js
+++ module.js
@@ -1,5 +1,6 @@
 const burger = require('burgerjs');
     
 burger.module([
+    [{name: 'header'},{type: 'component'},{selector: 'nem-header'}],
     [{name: 'quienes-somos'},{type: 'page'},{order: '0'},{route: '/'}],
 ]);
