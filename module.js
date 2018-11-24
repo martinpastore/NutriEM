be28247ab6225e2b3ff1a51765445de327cf8e5b
Martin Pastore <martinpablopastore@gmail.com>
2018-11-24 10:28:52 -0300

    cards

--- module.js
+++ module.js
@@ -2,5 +2,10 @@ const burger = require('burgerjs');
     
 burger.module([
     [{name: 'header'},{type: 'component'},{selector: 'nem-header'}],
-    [{name: 'quienes-somos'},{type: 'page'},{order: '0'},{route: '/'}],
+    [{name: 'card'},{type: 'component'},{selector: 'nem-card'}],
+    [{name: 'quienes-somos'},{type: 'page'},{order: '0'},{route: '/quienes-somos'}],
+    [{name: 'servicios'},{type: 'page'},{order: '1'},{route: '/servicios'}],
+    [{name: 'empresa'},{type: 'page'},{order: '2'},{route: '/empresa'}],
+    [{name: 'novedades'},{type: 'page'},{order: '3'},{route: '/novedades'}],
+    [{name: 'contacto'},{type: 'page'},{order: '4'},{route: '/contacto'}],
 ]);
