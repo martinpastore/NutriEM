edfddc893a9a664679620611f19139287062d16e
Martin Pastore <martinpablopastore@gmail.com>
2018-11-27 15:31:14 -0300

    Added server and contact

---
+++ components/contact/contact.js
@@ -0,0 +1,26 @@
+$('.contact').submit(function(e) {
+    e.preventDefault();
+
+    const form = e.currentTarget.id;
+
+    const contact = {
+        empresa: $(`#${form} #inputEmpresa`).val(),
+        email: $(`#${form} #inputEmail`).val(),
+        telefono: $(`#${form} #inputTelefono`).val(),
+        direccion: $(`#${form} #inputDireccion`).val(),
+        barrio: $(`#${form} #inputBarrio`).val(),
+        mensaje: $(`#${form} #inputMensaje`).val()
+    };
+
+    httpRequest('POST', 'http://localhost:3008/contact', contact)
+        .then(
+            (data) => {
+
+            }
+        )
+        .catch(
+            (error) => {
+
+            }
+        )
+});
--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var endpoints = require('./endpoints');
 var router = express.Router();
+
+router.post('/contact', endpoints.sendEmail);
 
 module.exports = router;
