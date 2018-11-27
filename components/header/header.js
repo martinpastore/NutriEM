2bdd7fe3387a4bfd4f106899735be17dac3133a5
Martin Pastore <martinpablopastore@gmail.com>
2018-11-27 10:50:23 -0300

    mision vision valores objetivos

--- components/header/header.js
+++ components/header/header.js
@@ -1,3 +1,15 @@
 $('.navbar-nav>li>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+$('.dropdown-item').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+
+$('.navbar-collapse').on('shown.bs.collapse', function(e) {
+    $('.dropdown').dropdown('toggle', 'open').show();
+});
+
+function mapUrl(id) {
+    redirect('/empresa', {id: id}, 'empresa');
+}
---
+++ pages/empresa/empresa.js
@@ -0,0 +1,28 @@
+function empresaOnInit() {
+    switch(routeParams.id) {
+        case 'mision':
+            document.getElementById('mision').style.display = 'block';
+            document.getElementById('vision').style.display = 'none';
+            document.getElementById('valores').style.display = 'none';
+            document.getElementById('objetivos').style.display = 'none';
+            break;
+        case 'vision':
+            document.getElementById('mision').style.display = 'none';
+            document.getElementById('vision').style.display = 'block';
+            document.getElementById('valores').style.display = 'none';
+            document.getElementById('objetivos').style.display = 'none';
+            break;
+        case 'valores':
+            document.getElementById('mision').style.display = 'none';
+            document.getElementById('vision').style.display = 'none';
+            document.getElementById('valores').style.display = 'block';
+            document.getElementById('objetivos').style.display = 'none';
+            break;
+        case 'objetivos':
+            document.getElementById('mision').style.display = 'none';
+            document.getElementById('vision').style.display = 'none';
+            document.getElementById('valores').style.display = 'none';
+            document.getElementById('objetivos').style.display = 'block';
+            break;
+    }
+}
