6089966a09804ab561b233c49484869799a63c4e
Martin Pastore <martinpablopastore@gmail.com>
2018-12-04 20:34:24 -0300

    routes

--- server/routes.js
+++ server/routes.js
@@ -3,5 +3,6 @@ var endpoints = require('./endpoints');
 var router = express.Router();
 
 router.post('/contact', endpoints.sendEmail);
+router.get('/news', endpoints.getNews);
 
 module.exports = router;
