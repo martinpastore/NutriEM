8765bdf654937875e947db12565d75b5a3d3ae18
Martin Pastore <martinpablopastore@gmail.com>
2018-12-10 09:24:31 -0300

    spinner

--- pages/novedades/novedades.js
+++ pages/novedades/novedades.js
@@ -1,10 +1,12 @@
 function novedadesOnInit() {
+    $('.spinner').removeClass('hide');
     if ($('.novedades__content-new')) {
         $('.novedades__content-new').remove();
     }
     httpRequest('GET', '/news')
         .then(
             (response) => {
+                let c = 0;
                 response = JSON.parse(response);
                 for (let i in response.data) {
                     const info = JSON.parse(response.data[i]);
@@ -15,8 +17,15 @@ function novedadesOnInit() {
                 <h5 class="novedades__text-title">${info.titulo}</h5>
                 <p class="novedades__text-content" id="novedadesText">${info.contenido}</p>
             </div>`);
+                        c++;
                     }
                 }
+
+                if (c < 0) {
+                    $('#novedades').append(`<span style="width:100%;float:left;text-align:center;padding: 3rem 0;">Todavía no hay novedades.</span>`);
+                }
+
+                $('.spinner').addClass('hide');
             }
         )
 }
