6fc530e5befe3bbcef4929c78a0bc1a3e7cb394f
Martin Pastore <martinpablopastore@gmail.com>
2019-01-14 10:12:06 -0300

    no image opton

--- pages/novedades/novedades.js
+++ pages/novedades/novedades.js
@@ -13,12 +13,20 @@ function novedadesOnInit() {
                     const info = JSON.parse(response.data[i]);
                     info.contenido = info.contenido.replace(/\s\s+/g, '<br>');
                     if (info.fecha) {
-                        $('#novedades').append(`<div class="novedades__content-new">
-                <span>${info.fecha}</span>
-                <img id="novedadesImagen" class="novedades__image" src="${info.image}" alt="">
-                <h5 class="novedades__text-title">${info.titulo}</h5>
-                <p class="novedades__text-content" id="novedadesText">${info.contenido}</p>
-            </div>`);
+                        if (info.image) {
+                            $('#novedades').append(`<div class="novedades__content-new">
+                                <span>${info.fecha}</span>
+                                <img id="novedadesImagen" class="novedades__image" src="${info.image}" alt="">
+                                <h5 class="novedades__text-title">${info.titulo}</h5>
+                                <p class="novedades__text-content" id="novedadesText">${info.contenido}</p>
+                            </div>`);
+                        } else {
+                            $('#novedades').append(`<div class="novedades__content-new">
+                                <span>${info.fecha}</span>
+                                <h5 class="novedades__text-title">${info.titulo}</h5>
+                                <p class="novedades__text-content" id="novedadesText">${info.contenido}</p>
+                            </div>`);
+                        }
                         c++;
                     }
                 }
